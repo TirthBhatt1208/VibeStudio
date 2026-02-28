3def5ed0cb6d72e3beb83e09669d57a94d8bcaf2
Tirth Bhatt <tirth.bhatt1208@gmail.com>
2026-02-28 12:49:31 +0530

    feat: integrate Clerk for authentication and user onboarding
    - Added @clerk/nextjs dependency to package.json and package-lock.json.
    - Updated Prisma schema to specify output directory for generated client.
    - Created RootLayout component to handle user onboarding on root layout.
    - Implemented Home page with a button and Clerk's UserButton for user management.
    - Added SignIn and SignUp pages using Clerk's components for authentication.
    - Wrapped application in ClerkProvider for authentication context.
    - Established database connection using Prisma with PostgreSQL adapter.
    - Implemented user onboarding logic in actions module to sync Clerk users with the database.
    - Set up middleware for protecting routes and managing public access.

--- src/app/(root)/page.jsx
+++ src/app/(root)/page.jsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
    <div className="flex items-center justify-center h-screen">
     <Button>Hello</Button>
+    <UserButton/>
    </div>
   );
 }
